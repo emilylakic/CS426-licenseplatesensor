fe70e41a200845e19066b05364dceca66dca8db3
emilylakic <emilylakic@gmail.com>
2019-12-06 09:18:55 -0500

    Documented insertcardata.java

--- app/src/main/java/com/example/myapplication/licenseplatesensor/insertcardata.java
+++ app/src/main/java/com/example/myapplication/licenseplatesensor/insertcardata.java
@@ -14,17 +14,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class insertcardata extends AppCompatActivity {
 
+    //EditText field allows ability to change the parameters of the name, plate number, car color, and car model
     EditText txtName, plateNumber, carColor, carModel;
+    //Saves user-inputted information
     Button btnSave;
+    //Takes user to the next page if an unrecognized vehicle is suspected
     Button nextPage;
+    //Two database references
     DatabaseReference reff;
     DatabaseReference reff2;
+    //Reference to Member class
     Member member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Corresponding activity layout
         setContentView(R.layout.activity_insertcardata);
+        //Must findViewById each object to be able to use them
         nextPage = (Button) findViewById(R.id.nextPage);
         txtName = (EditText)findViewById(R.id.txtName);
         plateNumber = (EditText)findViewById(R.id.plateNumber);
@@ -32,8 +39,11 @@ public class insertcardata extends AppCompatActivity {
         carColor = (EditText)findViewById(R.id.carColor);
         btnSave = (Button)findViewById(R.id.btnSave);
         member = new Member();
+        //Access child Member and under each unique key, access the key value pairs under that child in the database
         reff = FirebaseDatabase.getInstance().getReference().child("Member");
+        //Access child Unrecognized and under each unique key, access the key value pairs under that child in the database
         reff2 = FirebaseDatabase.getInstance().getReference().child("Unrecognized");
+        //Uses setter methods from Member class with values inputted by the user
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View view) {
@@ -42,15 +52,15 @@ public class insertcardata extends AppCompatActivity {
                 member.setModel(carModel.getText().toString().trim());
                 member.setColor(carColor.getText().toString().trim());
                 reff.push().setValue(member);
+                //Informs user a new member was added
                 Toast.makeText(insertcardata.this, "New Member Added", Toast.LENGTH_LONG).show();
             }
         });
+        //Navigates user to unrecognized vehicle screen (LoadImage class)
         nextPage.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-
                 Intent intent = new Intent(insertcardata.this, LoadImage.class);
-
                 startActivity(intent);
             }
         });
